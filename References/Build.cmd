6ca97a98c485a540231c8f2ef8dde7a51f5567ed
Lord-Giganticus <mrubins64@gmail.com>
2021-03-15 14:44:24 -0600

    add references

---
+++ References/Build.cmd
@@ -0,0 +1,14 @@
+git submodule init
+git submodule update
+cd SuperBMD
+git pull https://github.com/Sage-of-Mirrors/SuperBMD.git master
+nuget restore
+msbuild SuperBMD.sln -p:Configuration=Release
+cd ..\
+copy "SuperBMD\SuperBMDLib\bin\Release\SuperBMDLib.dll" %CD%
+cd GL_EditorFramework
+git pull https://github.com/jupahe64/GL_EditorFramework.git master
+nuget restore
+msbuild GL_EditorFramework.sln -p:Configuration=Release
+cd ..\
+copy "GL_EditorFramework\Gl_EditorFramework\bin\Release\GL_EditorFramework.dll" %CD%
