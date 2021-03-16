95d3f4f67fc446b7b9213036c2c35bbee83bdc10
Lord-Giganticus <mrubins64@gmail.com>
2021-03-16 08:43:27 -0600

    change how the references are updated

--- References/Update.cmd
+++ References/Update.cmd
@@ -3,15 +3,12 @@ git submodule update
 cd SuperBMD
 git pull https://github.com/Sage-of-Mirrors/SuperBMD.git master
 nuget restore
-msbuild SuperBMD.sln -p:Configuration=Release
 cd ..\
-copy "SuperBMD\SuperBMDLib\bin\Release\SuperBMDLib.dll" %CD%
 cd Spotlight
+git pull https://github.com/jupahe64/Spotlight.git master
 nuget restore
 cd ..\
 cd GL_EditorFramework
 git pull https://github.com/jupahe64/GL_EditorFramework.git master
 nuget restore
-msbuild GL_EditorFramework.sln -p:Configuration=Release
 cd ..\
-copy "GL_EditorFramework\Gl_EditorFramework\bin\Release\GL_EditorFramework.dll" %CD%
