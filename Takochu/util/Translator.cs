1fd0bd536506a49194508796c79905a71d81aba6
ぺんぐいん <ikikaerug10041@yahoo.co.jp>
2021-11-20 02:12:05 +0900

    Add MessBoxTranslator class.

--- Takochu/util/Translator.cs
+++ Takochu/util/Translator.cs
@@ -4,8 +4,80 @@ using System.IO;
 
 namespace Takochu.util
 {
+    public interface IMessageBoxLanguage
+    {
+
+        string GetMessage(MessageBoxTranslator.MessageBoxName messageBoxName);
+
+    }
+
+    // This class is for translating message boxes.
+    // In order to support multiple languages in the future,
+    // all languages inherit from the "IMessageBoxLanguage" interface.
+    public class MessageBoxTranslator
+    {
+        private readonly IMessageBoxLanguage _messageBoxLanguage;
+        public enum MessageBoxName 
+        {
+            InvalidFolder
+        }
+        public MessageBoxTranslator()
+        {
+            var TranslateName = 
+                (Translator.Language)System.Enum.Parse(typeof(Translator.Language), Properties.Settings.Default.Translation);
+            switch(TranslateName)
+            {
+                case Translator.Language.Japanese:
+                    _messageBoxLanguage = new MessageBoxJapanese();
+                    break;
+                case Translator.Language.English:
+                    _messageBoxLanguage = new MessageBoxEnglish();
+                    break;
+                case Translator.Language.German:
+                case Translator.Language.French:
+                case Translator.Language.Korean:
+                case Translator.Language.Spanish:
+                default:
+                    _messageBoxLanguage = new MessageBoxEnglish();
+                    break;
+            }
+        }
+
+        public string GetMessage(MessageBoxTranslator.MessageBoxName messageBoxName)
+        {
+            return _messageBoxLanguage.GetMessage(messageBoxName);
+        }
+    }
+
+    public class MessageBoxEnglish : IMessageBoxLanguage
+    {
+        private readonly string[]
+            MessageArray =
+            {
+                "Invalid folder. If you have already selected a correct folder, that will continue to be your base folder."
+            };
+        public string GetMessage(MessageBoxTranslator.MessageBoxName messageBoxName)
+        {
+            return MessageArray[((int)messageBoxName)];
+        }
+    }
+
+    public class MessageBoxJapanese : IMessageBoxLanguage
+    {
+        private readonly string[]
+            MessageArray =
+            {
+                "無効なフォルダです。すでに正しいフォルダを選択している場合は、そのフォルダが引き続きベースフォルダとなります。"
+            };
+        public string GetMessage(MessageBoxTranslator.MessageBoxName messageBoxName)
+        {
+            return MessageArray[((int)messageBoxName)];
+        }
+    }
+
     public class Translator
     {
+
         public enum Language
         {
             English,
@@ -19,7 +91,7 @@ namespace Takochu.util
         private enum LangageCode
         {
             en,
-            du,
+            de,
             jp
         }
 
@@ -33,10 +105,11 @@ namespace Takochu.util
             { "Spanish", Language.Spanish }
         };
 
-        public Translator(string lang)
+        public Translator()
         {
-            mLanguage = sStringToLang[lang];
+            mLanguage = sStringToLang[Properties.Settings.Default.Translation];
         }
+
 
         /*public string GetTranslation(string where, string what)
         {
@@ -53,7 +126,7 @@ namespace Takochu.util
                     break;
 
                 case Language.German:
-                    path = JointSimpleGalaxyName(LangageCode.du);
+                    path = JointSimpleGalaxyName(LangageCode.de);
                     break;
 
                 case Language.Japanese:
@@ -66,14 +139,11 @@ namespace Takochu.util
             }
             Properties.Settings.Default.Translation = mLanguage.ToString();
             Properties.Settings.Default.Save();
-            System.Console.WriteLine(Properties.Settings.Default.Translation);
-            System.Console.WriteLine( path);
-            System.Console.WriteLine(mLanguage.ToString());
             string[] lines = File.ReadAllLines(path);
 
             Dictionary<string, string> ret = new Dictionary<string, string>();
 
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 string[] split = line.Split('=');
                 ret.Add(split[0], split[1]);
@@ -81,8 +151,8 @@ namespace Takochu.util
 
             return ret;
         }
-        
-        private string JointSimpleGalaxyName(LangageCode lc) 
+
+        private string JointSimpleGalaxyName(LangageCode lc)
         {
             return $"res/translations/{lc}/SimpleGalaxyNames_{(GameUtil.IsSMG1() ? "SMG1.txt" : "SMG2.txt")}";
         }
